45f039fc1a0ba3c46cd4d660faefb80ad44f43b8
professorVoid <thecalebwoods@gmail.com>
2023-02-17 17:41:50 -0500

    Add the game loop to my platformer

--- src/Main/Game.java
+++ src/Main/Game.java
@@ -1,7 +1,9 @@
 package Main;
 
-public class Game {
+public class Game implements Runnable{
 
+    private Thread gameThread;
+    private final int FPS_SET =120;
     private GameWindow gameWindow;
     //testing
     private GamePanel gamePanel;
@@ -11,6 +13,40 @@ public class Game {
         gameWindow = new GameWindow(gamePanel);
         gamePanel.setFocusable(true);
         gamePanel.requestFocus(); // gets input focus
+        startGameLoop();
 
+    }
+
+    private void startGameLoop(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        double timePerFrame = 1000000000.0 / FPS_SET;
+
+        long lastFrame = System.nanoTime();
+        long now;
+        int frames = 0;
+        long lastCheck = System.currentTimeMillis();
+
+        while(true){
+            now = System.nanoTime();
+            if(System.nanoTime() - lastFrame >= timePerFrame)
+            {
+                gamePanel.repaint();
+                lastFrame = now;
+                frames++;
+            }
+
+
+            if (System.currentTimeMillis() - lastCheck >= 1000) {
+                lastCheck = System.currentTimeMillis();
+                System.out.println("FPS: " + frames);
+                frames = 0;
+            }
+
+        }
     }
 }
--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -5,11 +5,17 @@ import Inputs.MouseInputs;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 public class GamePanel extends JPanel {
 
     private MouseInputs mouseInputs;
-    private int xDelta = 100, yDelta = 100;
+    private float xDelta = 100.0F, yDelta = 100.0F;
+    private float xDir = 1F, yDir = 1F;
+    private Color color = new Color(100,100,100);
+    private Random random = new Random();
+    private int frames;
+    private long lastCheck;
     public GamePanel(){
 
         mouseInputs = new MouseInputs(this);
@@ -21,26 +27,44 @@ public class GamePanel extends JPanel {
 
     public void changeXDelta(int value){
         this.xDelta += value;
-        repaint();
     }
 
     public void changeYDelta(int value){
         this.yDelta += value;
-        repaint();
     }
 
     public void setRectPos(int x, int y){
         this.xDelta = x;
         this.yDelta = y;
-        repaint();
     }
 
     public void paintComponent(Graphics g){
         super.paintComponent(g); // JComponent method
+        updateRectangle();
+        //g.drawRect(100, 100, 200, 50)
+        g.setColor(color);
+        g.fillRect((int) xDelta, (int) yDelta, 200, 50);
+    }
 
-        //g.drawRect(100, 100, 200, 50);
-        g.fillRect(xDelta, yDelta, 200, 50);
+    public void updateRectangle(){
+        xDelta += xDir;
+        yDelta += yDir;
+        if (xDelta > 400 || xDelta < 0){
+            xDir *= -1;
+            color = getRandColor();
+        }
+        if (yDelta > 400 || yDelta < 0){
+            yDir *= -1;
+            color = getRandColor();
+        }
+    }
 
+    public Color getRandColor(){
+        int r = random.nextInt(255);
+        int g = random.nextInt(255);
+        int b = random.nextInt(255);
+
+        return new Color(r,g,b);
     }
 
 }
